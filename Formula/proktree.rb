01eeaa78fa5c349d26dcfaf0d679fce5ba69b22f
Jeremy Wohl <jw-github@igmus.org>
2025-07-13 19:39:47 -0400

    start

---
+++ Formula/proktree.rb
@@ -0,0 +1,23 @@
+class Proktree < Formula
+  desc "Print your processes as a tree, nicely displayed"
+  homepage "https://github.com/jeremywohl/proktree"
+  url "https://github.com/jeremywohl/proktree/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"proktree"
+    man1.install "proktree.1"
+  end
+
+  test do
+    # Test that the binary runs and shows help
+    assert_match "Print your processes as a tree", shell_output("#{bin}/proktree --help")
+    
+    # Test basic functionality (will show at least the shell process)
+    output = shell_output("#{bin}/proktree")
+    assert_match(/PID\s+USER\s+%CPU\s+%MEM\s+RSS\s+START\s+TIME\s+COMMAND/, output)
+  end
+end
