09b53e8ef9a75bf5283cb56371ecf6ab8a45f87a
Jeremy Wohl <jw-github@igmus.org>
2026-07-30 20:05:25 -0400

    makedog v0.5.0: real tarball checksum

--- Formula/makedog.rb
+++ Formula/makedog.rb
@@ -2,7 +2,7 @@ class Makedog < Formula
   desc "Runner for server binaries: restart on builds, log runs, send signals"
   homepage "https://github.com/jeremywohl/makedog"
   url "https://github.com/jeremywohl/makedog/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "7c98510185b8390774000af8939c452b310d31ed8a6b9cff5e380954c1627e73"
   license "MIT"
 
   depends_on "go" => :build
